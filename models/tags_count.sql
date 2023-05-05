d4a9cb86450da8df53af56eb8244b8ce65c63e6b
hh8@nimbusintelligence.com <hh8@nimbusintelligence.com>
2023-05-05 10:24:56 +0000

    tags_count with quarters etc.

--- models/tags_count.sql
+++ models/tags_count.sql
@@ -15,12 +15,15 @@ WITH Tags AS (
 tags_count AS (
     SELECT DISTINCT
         LISTAGG(video_id, ', ') WITHIN GROUP (ORDER BY trending_date DESC) OVER (PARTITION BY stg_tags_list.Tag_Value)                 AS videos_id_list
+        , YEAR(trending_date)                   AS year
+        , QUARTER(trending_date)                AS quarter
+        , MONTH(trending_date)                  AS month
         , stg_tags_list.Tag_Value                                                                                             AS tag
-        , COUNT(*)                OVER (PARTITION BY tag)                                                                     AS tag_count
+        , COUNT(*)                OVER (PARTITION BY year, quarter, month, tag)                                                                     AS tag_count
         , MAX(stg_tags_list.trending_date) OVER (PARTITION BY tag)                                                            AS last_trend_date
     FROM stg_tags_list
 )
 SELECT *
 FROM tags_count
 WHERE tag !LIKE '[none]'
-ORDER BY tag_count DESC
+ORDER BY year, quarter, month, tag_count DESC
