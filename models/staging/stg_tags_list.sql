57d0829251f2c68de559db41d913bca8c2ddd4fe
hh8@nimbusintelligence.com <hh8@nimbusintelligence.com>
2023-05-05 07:48:38 +0000

    tags updated

--- models/staging/stg_tags_list.sql
+++ models/staging/stg_tags_list.sql
@@ -2,14 +2,14 @@ WITH Tags AS (
     SELECT
         Video_ID
       , Trending_Date
-      , index
+      --, index
       , Tags
     FROM {{ ref('video_info') }}
 )
 SELECT
       Video_ID
     , Trending_Date
-    , Tags.index
-    , Input.index     AS Tag_Index
-    , Input.value     AS Tag_Value
-FROM Tags, LATERAL split_to_table(Tags.Tags, ',') AS Input
+    --, Tags.index
+    , split_tags.index     AS Tag_Index
+    , split_tags.value     AS Tag_Value
+FROM Tags, LATERAL split_to_table(Tags.Tags, ',') AS split_tags
